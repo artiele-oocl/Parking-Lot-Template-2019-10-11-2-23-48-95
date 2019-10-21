c86cb875aa5f7541e0a31ab01822aac1bd6df8e4
Lester Jan Artienda <artiele@oocl.com>
2019-10-21 21:47:01 +0800

    Add updateParkingLot: user can only update capacity, not location or name

--- src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
+++ src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.NotSupportedException;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,6 +38,12 @@ public class ParkingLotController {
     @DeleteMapping(path = "/{name}")
     public ParkingLot deleteParkingLot(@PathVariable String name) throws NotFoundException {
         return parkingLotService.deleteParkingLot(name);
+    }
+    @ResponseStatus(code = HttpStatus.OK)
+    @PatchMapping(path = "/{name}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ParkingLot updateParkingLot(@RequestBody ParkingLot parkingLot, @PathVariable String name)
+            throws NotFoundException, NotSupportedException {
+        return parkingLotService.updateParkingLot(parkingLot, name);
     }
 
 }
--- src/main/java/com/thoughtworks/parking_lot/service/ParkingLotService.java
+++ src/main/java/com/thoughtworks/parking_lot/service/ParkingLotService.java
@@ -6,6 +6,7 @@ import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.NotSupportedException;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,5 +37,13 @@ public class ParkingLotService {
         if(!parkingLot.isPresent()) throw new NotFoundException(PARKING_NOT_FOUND);
         parkingLotRepository.delete(parkingLot.get());
         return parkingLot.get();
+    }
+    public ParkingLot updateParkingLot(ParkingLot parkingLot, String name) throws NotFoundException, NotSupportedException {
+        Optional<ParkingLot> parkingLotDB = Optional.ofNullable(parkingLotRepository.findOneByName(name));
+        if(parkingLot == null || !parkingLotDB.isPresent()) throw new NotFoundException(PARKING_NOT_FOUND);
+        if(parkingLot.getCapacity() < 1) throw new NotSupportedException(INVALID_CAPACITY);
+
+        parkingLotDB.get().setCapacity(parkingLot.getCapacity()); // you can only update capacity
+        return parkingLotRepository.save(parkingLotDB.get());
     }
 }
