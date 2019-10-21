a6a48baaf15c6f9f19bea07e2fb3a29f247f5c3c
Lester Jan Artienda <artiele@oocl.com>
2019-10-21 21:28:23 +0800

    Add getParkingLotByNameLike

--- src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
+++ src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
@@ -2,10 +2,14 @@ package com.thoughtworks.parking_lot.controller;
 
 import com.thoughtworks.parking_lot.entity.ParkingLot;
 import com.thoughtworks.parking_lot.service.ParkingLotService;
+import javassist.NotFoundException;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/parkingLots")
@@ -18,5 +22,15 @@ public class ParkingLotController {
     public Iterable<ParkingLot> getAllParkingLots(@RequestParam(required = false, defaultValue = "0") Integer page,
                                                  @RequestParam(required = false, defaultValue = "15") Integer pageSize) {
         return parkingLotService.getAllParkingLots(page, pageSize);
+    }
+    @ResponseStatus(code = HttpStatus.OK)
+    @GetMapping(path = "/{name}")
+    public Optional<ParkingLot> getParkingLotByName(@PathVariable String name) throws NotFoundException {
+        return parkingLotService.getParkingLotByName(name);
+    }
+    @ResponseStatus(code = HttpStatus.OK)
+    @GetMapping
+    public List<ParkingLot> getParkingLotByNameLike(@RequestParam(required = false) String name) throws NotFoundException {
+        return parkingLotService.getParkingLotByNameLike(name);
     }
 }
--- src/main/java/com/thoughtworks/parking_lot/service/ParkingLotService.java
+++ src/main/java/com/thoughtworks/parking_lot/service/ParkingLotService.java
@@ -2,15 +2,33 @@ package com.thoughtworks.parking_lot.service;
 
 import com.thoughtworks.parking_lot.entity.ParkingLot;
 import com.thoughtworks.parking_lot.repository.ParkingLotRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ParkingLotService {
     @Autowired
     private ParkingLotRepository parkingLotRepository;
 
+    private static final String PARKING_NOT_FOUND = "PARKING LOT NOT FOUND";
+    private static final String INVALID_CAPACITY = "INVALID CAPACITY";
+    private static final String PARKING_NAME_ALREADY_EXISTS = "PARKING NAME ALREADY EXISTS";
+
     public Iterable<ParkingLot> getAllParkingLots(Integer page, Integer pageSize) {
         return parkingLotRepository.findAll();
+    }
+    public Optional<ParkingLot> getParkingLotByName(String name) throws NotFoundException {
+        Optional<ParkingLot> parkingLot = Optional.ofNullable(parkingLotRepository.findOneByName(name));
+        if(!parkingLot.isPresent()) throw new NotFoundException(PARKING_NOT_FOUND);
+        return parkingLot;
+    }
+    public List<ParkingLot> getParkingLotByNameLike(String name) throws NotFoundException {
+        List<ParkingLot> parkingLot = parkingLotRepository.findByNameContaining(name);
+        if(parkingLot.size() == 0) throw new NotFoundException(PARKING_NOT_FOUND);
+        return parkingLotRepository.findByNameContaining(name);
     }
 }
