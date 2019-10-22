8a469df72b2fe2a8846aa5792fc2f6b991b2ba8a
Lester Jan Artienda <artiele@oocl.com>
2019-10-22 22:12:52 +0800

    Initialize ParkingLot for testing

--- src/test/java/com/thoughtworks/parking_lot/controller/ParkingLotControllerTest.java
+++ src/test/java/com/thoughtworks/parking_lot/controller/ParkingLotControllerTest.java
@@ -1,4 +1,36 @@
 package com.thoughtworks.parking_lot.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.parking_lot.entity.ParkingLot;
+import com.thoughtworks.parking_lot.service.ParkingLotService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ParkingLotController.class)
+@ActiveProfiles(profiles = "test")
 public class ParkingLotControllerTest {
+
+    @MockBean
+    ParkingLotService parkingLotService;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private ParkingLot createParkingLot(String name) {
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.setName(name);
+        parkingLot.setCapacity(100);
+        parkingLot.setLocation("BGC");
+
+        return parkingLot;
+    }
 }
