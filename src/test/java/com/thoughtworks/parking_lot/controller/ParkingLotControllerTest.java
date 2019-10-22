e8e7e0f70150c55a64f7f1ac1d30c43d55c79e89
Lester Jan Artienda <artiele@oocl.com>
2019-10-22 22:21:22 +0800

    Passed should_save_new_parkinglot_when_capacity_is_greater_than_1_and_parkinglot_is_new

--- src/test/java/com/thoughtworks/parking_lot/controller/ParkingLotControllerTest.java
+++ src/test/java/com/thoughtworks/parking_lot/controller/ParkingLotControllerTest.java
@@ -3,13 +3,20 @@ package com.thoughtworks.parking_lot.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.parking_lot.entity.ParkingLot;
 import com.thoughtworks.parking_lot.service.ParkingLotService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(ParkingLotController.class)
@@ -32,5 +39,19 @@ public class ParkingLotControllerTest {
         parkingLot.setLocation("BGC");
 
         return parkingLot;
+    }
+
+    @Test
+    void should_save_new_parkinglot_when_capacity_is_greater_than_1_and_parkinglot_is_new() throws Exception {
+        ParkingLot parkingLot = createParkingLot("ParkingLotTest");
+
+        when(parkingLotService.saveParkingLot(parkingLot)).thenReturn(parkingLot);
+
+        ResultActions resultOfExecution = mvc.perform(post("/parkingLots")
+                .content(objectMapper.writeValueAsString(parkingLot))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        resultOfExecution.andExpect(status().isCreated());
     }
 }
