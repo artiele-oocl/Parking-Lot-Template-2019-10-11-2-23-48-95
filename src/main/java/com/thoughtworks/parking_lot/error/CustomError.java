60f92f64356bfa391b8560e93528c3e420a8f664
Lester Jan Artienda <artiele@oocl.com>
2019-10-22 21:57:49 +0800

    CustomError add properties and methods

--- src/main/java/com/thoughtworks/parking_lot/error/CustomError.java
+++ src/main/java/com/thoughtworks/parking_lot/error/CustomError.java
@@ -1,4 +1,25 @@
 package com.thoughtworks.parking_lot.error;
 
 public class CustomError {
+    private String errMessage;
+    private Integer errCode;
+
+    public CustomError() {
+    }
+
+    public String getErrMessage() {
+        return errMessage;
+    }
+
+    public void setErrMessage(String errMessage) {
+        this.errMessage = errMessage;
+    }
+
+    public Integer getErrCode() {
+        return errCode;
+    }
+
+    public void setErrCode(Integer errCode) {
+        this.errCode = errCode;
+    }
 }
