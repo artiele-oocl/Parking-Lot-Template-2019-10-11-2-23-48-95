317d9c15706cda37b8ffb08a2741157b64e227f7
Lester Jan Artienda <artiele@oocl.com>
2019-10-22 22:04:09 +0800

    Add NotFoundExceptionHandler

--- src/main/java/com/thoughtworks/parking_lot/handler/ControllerExceptionHandler.java
+++ src/main/java/com/thoughtworks/parking_lot/handler/ControllerExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.thoughtworks.parking_lot.handler;
 
+import com.thoughtworks.parking_lot.error.CustomError;
+import javassist.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class ControllerExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public CustomError NotFoundExceptionHandler(NotFoundException e) {
+        CustomError customError = new CustomError();
+        customError.setErrCode(HttpStatus.NOT_FOUND.value());
+        customError.setErrMessage(e.getMessage());
+        return customError;
+    }
 }
